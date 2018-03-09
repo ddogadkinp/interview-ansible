62fcba63bdd83631e7ddc39705400df78647c25c
EC2 Default User <ec2-user@ip-172-31-47-98.us-east-2.compute.internal>
2018-03-09 17:51:51 +0000

    Added implementation

---
+++ files/pm2-ecosystem-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps : [{
+    name   : "server3000",
+    script : "/home/ec2-user/environment/server3000/index.js"
+  },{
+    name   : "server3001",
+    script : "/home/ec2-user/environment/server3001/index.js"
+  },{
+    name   : "server3002",
+    script : "/home/ec2-user/environment/server3002/index.js"
+  }]
+}
